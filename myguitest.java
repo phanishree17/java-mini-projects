60f2139ea01461711d288677bc5238277ebfee2d
Phanishree NM <phanishreenm@gmail.com>
2026-02-05 10:46:34 +0530

    Add files via upload

---
+++ myguitest.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class TestGUI {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Hello");
+        f.setSize(300,200);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
